5d6200a94587a3ad945bf02f9dfb7bfb9cd767df
inno <innocentiahan14@gmail.com>
2025-09-04 20:40:12 +0800

    push base

---
+++ bin/practicing_dart.dart
@@ -0,0 +1,5 @@
+import 'package:practicing_dart/practicing_dart.dart' as practicing_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${practicing_dart.calculate()}!');
+}
---
+++ test/practicing_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:practicing_dart/practicing_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
